2fd38fdc18c3afc87e4865e3e34919c335fb726e
sunilkumar060 <sunil.kumar@moonraft.com>
2018-09-10 17:10:40 +0530

    css modification

--- JS/bank.js
+++ JS/bank.js
@@ -28,28 +28,51 @@
                 });
               }
             }
-            function tickMark(){
-                // var widget = document.getElementById("tick");
+              function tickMark(){
+                // var widget = document.getElementById("tick")
                 var widgets = document.getElementsByClassName("widget-content");
                 for (var i = 0; i < widgets.length; i++) {
                 widgets[i].addEventListener("click", function() {
                 var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                /*this.className += " active";*/
-                this.classList.toggle("active");
-                    });
-                  }
-                }
-            function widgetBarFunction(){
-                var widget=document.getElementById("myWidget").classList.toggle('widget-container')
+                current[0].className = current[0].className.replace("active"," ");
+                // this.className +=" active ";
+                 this.classList.toggle("active");
+                });
+              }
+            }
+              function widgetBarFunction(){
+                var widget=document.getElementById("myWidget").classList.toggle('widgets-container')
                 if(widget==false){
-                console.log(document.getElementById("widgetDropdown").style.width="66%");
+                console.log(document.getElementById("widgetContainer").style.width="75%");
                 }
                 else{
-                  console.log(document.getElementById("widgetDropdown").style.width="90%");
+                  console.log(document.getElementById("widgetContainer").style.width="90%");
+                } 
+                widget=document.getElementById("myWidget");
+                window.onclick= function(){
+                  widget=document.getElementById("myWidget");
+                  if(!Event.target == widget){
+                    widget.style.display="none";
+                  }
+                 }
+            } 
+               
+
+              
+               /*function widgetBarFunction(){
+               window.onclick = function(widgetBarFunction) {
+                if (!widgetBarFunction.target.matches('.filter-dropdown')) {
+  
+                var widget = document.getElementsByClassName("myWidget");
+                if (widget==true) {
+                // var openDropdown = widget[i];
+                 widget.classList.contains('widgets-container') 
+                 widget.classList.remove('widgets-container');
                 }
+              }
             }
-           
+          }
+        
                
             
                 
